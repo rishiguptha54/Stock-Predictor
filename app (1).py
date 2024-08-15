c67564b486f500ede9986d449f00cee0df4f5a47
rishiguptha54 <156742792+rishiguptha54@users.noreply.github.com>
2024-08-16 01:07:52 +0530

    app.py

--- app (1).py
+++ app (1).py
@@ -7,33 +7,33 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 
-# Load the Keras model
+
 model = load_model(r"/content/Stock Predictions Model.keras")
 
 st.header('Stock Market Predictor')
 
-# Get user input for the stock symbol
+
 stock = st.text_input('Enter Stock Symbol', 'GOOG')
 start = '2012-01-01'
-end = '2022-12-31'
+end = '2024-06-04'
 
-# Fetch the stock data
+
 data = yf.download(stock, start, end)
 
 st.subheader('Stock Data')
 st.write(data)
 
-# Split the data into training and testing sets
+
 data_train = pd.DataFrame(data.Close[0: int(len(data) * 0.80)])
 data_test = pd.DataFrame(data.Close[int(len(data) * 0.80): len(data)])
 
-# Scale the data
+
 scaler = MinMaxScaler(feature_range=(0, 1))
 pas_100_days = data_train.tail(100)
 data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
 data_test_scale = scaler.fit_transform(data_test)
 
-# Plotting Price vs MA50
+
 st.subheader('Price vs MA50')
 ma_50_days = data.Close.rolling(50).mean()
 fig1 = plt.figure(figsize=(8, 6))
@@ -43,7 +43,7 @@ plt.legend()
 plt.show()
 st.pyplot(fig1)
 
-# Plotting Price vs MA50 vs MA100
+
 st.subheader('Price vs MA50 vs MA100')
 ma_100_days = data.Close.rolling(100).mean()
 fig2 = plt.figure(figsize=(8, 6))
@@ -54,7 +54,7 @@ plt.legend()
 plt.show()
 st.pyplot(fig2)
 
-# Plotting Price vs MA100 vs MA200
+
 st.subheader('Price vs MA100 vs MA200')
 ma_200_days = data.Close.rolling(200).mean()
 fig3 = plt.figure(figsize=(8, 6))
@@ -65,7 +65,7 @@ plt.legend()
 plt.show()
 st.pyplot(fig3)
 
-# Prepare data for prediction
+
 x = []
 y = []
 
@@ -75,15 +75,15 @@ for i in range(100, data_test_scale.shape[0]):
 
 x, y = np.array(x), np.array(y)
 
-# Make predictions
+
 predictions = model.predict(x)
 
-# Reverse scaling
+
 scale = 1 / scaler.scale_[0]
 predictions = predictions * scale
 y = y * scale
 
-# Plotting Original Price vs Predicted Price
+
 st.subheader('Original Price vs Predicted Price')
 fig4 = plt.figure(figsize=(8, 6))
 plt.plot(y, 'r', label='Original Price')
